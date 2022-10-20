8655edba8255e00b6e40b5fe26437e26081bd364
s64309010024 <s64309010024@ptnicec.ac.th>
2022-10-20 12:29:26 +0700

    ส่ง variable

---
+++ datatype.php
@@ -0,0 +1,31 @@
+<?php
+
+    //string
+    $grade = "A";
+    $subject = "HTML";
+    $name = 'Jindarat';
+    $age = "22";
+
+    //Integer Nuber
+    $timeout = 30;
+    $score = 99;
+    $loan = -19;
+    $money = +88;
+    
+    //Floating point
+    $gpa = 3.33;
+    $inits = 0.25;
+    $temp = 2.22;
+    $lumen = +7600.99;
+    $maths = 2e10;
+    echo $maths;
+
+    //Boolean
+    $famale = true;
+    $male = false;
+
+    //
+
+
+
+?>
---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+
+    $fname = 2;
+    $a1111 = 4;
+    $_test01 = 3.33;
+    $Age = 100;
+    $age = 99;
+    $fullname = "Jindarat ptcharat";
+    $prefix = "Mr.";
+
+    echo $fname."<br>";
+    echo $a1111;
+    echo "<br>".$Age."<br>";
+    echo $prefix. $fullname."<br>";
+    echo $Age + $age."<br>";
+    echo $Age." + ".$age." = ".($Age + $age);   
+    print "<br>".$_test01;
+    
+
+
+?>
