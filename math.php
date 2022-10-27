402ba477fdef0d16224686b8853084665f012633
s64309010024 <s64309010024@ptnicec.ac.th>
2022-10-27 11:17:29 +0700

    ส่งครั้งที่ 2

---
+++ math.php
@@ -0,0 +1,19 @@
+<?php
+
+    $num1 = pi()*(2*2);
+    echo $num1;
+    
+    $ages = array(19,20,22,15,16,17,25,20,14);
+    echo "<br>";
+    echo min($ages);
+    echo "<br>";
+    echo(max($ages));
+    echo "<br>";
+    echo sqrt(121);
+    echo "<br>";
+    echo round(1.53);
+    echo "<br>";
+    echo rand(111,222);
+
+
+?>
